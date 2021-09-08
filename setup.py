e40ee61847ab9f2fd60b67ae0ce66baa02199626
Michael Lucy <mlucy@stripe.com>
2021-09-08 15:54:59 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
 setup(name='kalshi',
-      version='0.1.0',
+      version='0.1.1',
       description='Library for accessing Kalshi API',
-      url='http://github.com/STUBBED',
+      url='https://github.com/mlucy/kalshi_py',
       author='Michael Lucy',
       author_email='michaelglucy@gmail.com',
       license='MIT',
       packages=['kalshi'],
       zip_safe=True,
 
-      tests_reqire=['nose'],
+      install_requires=['requests'],
+      python_requires='>=3',
 )
